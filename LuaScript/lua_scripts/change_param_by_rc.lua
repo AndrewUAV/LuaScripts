efc4a4aad59d25795a7d4bce291a2688e16800df
AndrewUAV <145654146+AndrewUAV@users.noreply.github.com>
2025-05-12 10:28:23 +0300

    Update change_param_by_rc.lua

--- LuaScript/lua_scripts/change_param_by_rc.lua
+++ LuaScript/lua_scripts/change_param_by_rc.lua
@@ -1,38 +1,37 @@
-local socket = require("socket")
 
 -- номер каналу, значення якого ми будемо перевіряти
-
 local rc_channel_num = 7
 
-local delay_main_function = 1
-local delay_write_params = 0.02
+-- значення pwm для каналу в яких межах, буде виконуватись функція
+local pwm_min = 900
+local pwm_trim = 1500
 
+local delay_main_function = 1000
 local default_params_flag = false
 local new_params_flag = true
 
 
 -- словник параметрів для дефолтних значень
--- щоб додати свої параметри, просто додайте їх черз кому в словник - ["<Назва параметру>"] = <значення параметру>
-
+-- щоб додати свої параметри, просто додайте їх через кому в словник - ["<Назва параметру>"] = <значення параметру>
 local default_params_dict = {
     ["AHRS_TRIM_X"] = 0,
     ["AHRS_TRIM_Y"] = 0,
+    ["PTCH_TRIM_DEG"] = 0
 }
 
 -- словник параметрів для зміни даних
 -- щоб додати свої параметри, просто додайте їх черз кому в словник - ["<Назва параметру>"] = <значення параметру>
-
 local new_params_dict = {
-    ["AHRS_TRIM_X"] = 3.14,
+    ["AHRS_TRIM_X"] = 0,
     ["AHRS_TRIM_Y"] = 3.14,
+    ["PTCH_TRIM_DEG"] = 20
 }
 
 
--- функція для перевірки стану каналу rc, який буде давати ШІМ. Повертає true/false
 function check_lvl_rc()
-    local pwm_value = get_pwm(rc_channel_num)
-    gcs:send_text(pwm_value)
-    if 900 < pwm_value < 1500 then
+    local pwm_value = rc:get_pwm(rc_channel_num)
+    --gcs:send_text(6, pwm_value)
+    if pwm_value > pwm_min and pwm_value < pwm_trim then
         return false
     else
         return true
@@ -40,40 +39,41 @@ function check_lvl_rc()
 end
 
 
--- функція для зміни параметрів на нові
 function update_params(status)
     if status and new_params_flag then
-        --for key, value in pairs(new_params_dict) do
-          --  print(key, value)
-        --end
-        print("update_params")
-        gcs:send_text("update_params")
+
+        for key, value in pairs(new_params_dict) do
+            param:set(key, value)
+        end
+
+        gcs:send_text(6, "LUA: UPDATE PARAMS")
         new_params_flag = false
         default_params_flag = true
     end
 end
 
 
--- функція для повернення параметрів до дефолтних значень
 function return_2_default(status)
     if status == false and default_params_flag then
-        print("return_2_default")
-        gcs:send_text("return_2_default")
 
+        for key, value in pairs(default_params_dict) do
+            param:set(key, value)
+        end
+
+        gcs:send_text(6, "LUA: RETURN TO DEFAULT")
         new_params_flag = true
         default_params_flag = false
     end
 end
 
 
--- головна функція скрипта, де перевіряється значення rc та виконує певну дію в залежності від значення
 function main()
+    status = check_lvl_rc()
+    update_params(status)
+    return_2_default(status)
 
-    gcs:send_text(0, "run script")
-
-    return main, 1000
+    return main, delay_main_function
 end
 
 
--- запускаємо функцію main
-main()
+return main(), delay_main_function
